80fb9127420c39ae31f480bfda5b4d6840ae5b1d
Akshaya Ma <akshai2288@gmail.com>
2016-08-08 15:29:46 +0530

    Add files via upload

---
+++ one.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone1
+{
+ 	public static void main (String[] args) throws java.lang.Exception
+ 	{
+    	
+  		Scanner in=new Scanner(System.in);
+    		int a1,i;
+    		a1=in.nextInt();
+  		int a[]=new int[t];
+  		System.out.println(t);
+  		for(i=0;i<t;i++)
+  		{
+   			a[i]=in.nextInt();
+  		}
+   	
+ 			Arrays.sort(a);
+   	System.out.println(a[t-5]);
+  		
+ 	}
+}
